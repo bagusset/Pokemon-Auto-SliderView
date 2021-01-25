5927e168a7e0f4c3abe552505eff99a41ce769ad
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-01-25 13:04:00 +0700

    add files!

---
+++ Pokemon-Auto-SliderView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon-Auto-SliderView
+//
+//  Created by Bagus setiawan on 25/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
